455de0925a7fc8fdcb120a6ca107abe59d969d7d
Mario Aragao <marioestudos.nuvem@gmail.com>
2024-09-10 01:56:57 -0300

    loginPage and Navbar

---
+++ front-end/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import AppNavBar from './components/AppNavBar.vue'
+</script>
+
+<template>
+  <nav>
+    <AppNavBar />
+  </nav>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ front-end/src/views/LoginView.vue
@@ -0,0 +1,40 @@
+<template >
+  <div class="d-flex justify-content-center align-items-center vh-100 bg-light" >
+    <form class="p-4 bg-white border rounded shadow-sm" style="width: 320px;">
+      <img src="../img/aplicativo-web.png" class="img-fluid mx-auto d-block mb-3" alt="Imagem de Login" style="height: 80px; width: 80px;" />
+      <div class="form-group mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          placeholder="Digite seu e-mail"
+        />
+      </div>
+      <div class="form-group mb-3">
+        <label for="exampleInputPassword1" class="form-label">Senha</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+          placeholder="Digite sua senha"
+        />
+      </div>
+      <div class="form-check mb-3">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Lembrar-me</label>
+      </div>
+      <button type="submit" class="btn btn-primary w-100">Entrar</button>
+    </form>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+/* Estilos mínimos apenas para imagem */
+img {
+  max-height: 80px;
+  max-width: 80px;
+}
+</style>
